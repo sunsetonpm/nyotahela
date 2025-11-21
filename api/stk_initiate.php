8f91e585b3a5d9a62dbde3f40d9e83cd7e31124b
Calton Korir <kcaltoncheruiyot@gmail.com>
2025-11-21 16:27:47 +0300

    Bug fix

--- api/stk_initiate.php
+++ api/stk_initiate.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
